da3cb6101ec1256fd23eb244b32a2309e01ee661
adityakishore25 <kishoreaditya270@gmail.com>
2023-07-23 19:53:19 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,21 @@
+// code everyday :), otherwise :(
+// Aditya Kishore
+#include <bits/stdc++.h>
+using namespace std;
+
+void code()
+{
+    
+}
+
+int main()
+{
+    int girlfriend = 16400;
+    cin >> girlfriend;
+    while (girlfriend--)
+    {
+        code();
+        cout << endl;
+    }
+    return 0;
+}
