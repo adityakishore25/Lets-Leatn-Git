112e0aa9c9d6582a89468d2c5a1b4d2a696299ad
adityakishore25 <kishoreaditya270@gmail.com>
2023-07-23 15:40:48 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+// code everyday :), otherwise :(
+// Aditya Kishore
+#include <bits/stdc++.h>
+using namespace std;
+
+void code()
+{
+    int a = 5, b = 4;
+    cout << a+b;
+}
+
+int main()
+{
+    code();
+    return 0;
+}
