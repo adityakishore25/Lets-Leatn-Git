fc8a27571f1fc8c4b838aeb77a97ef21329b049f
adityakishore25 <kishoreaditya270@gmail.com>
2023-07-23 21:33:31 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,22 @@
+// code everyday :), otherwise :(
+// Aditya Kishore
+#include <bits/stdc++.h>
+using namespace std;
+
+void code()
+{
+    int a = 9, b = 5;
+    cout << "The product is " << (a * b);
+}
+
+int main()
+{
+    int girlfriend = 16400;
+    cin >> girlfriend;
+    while (girlfriend--)
+    {
+        code();
+        cout << endl;
+    }
+    return 0;
+}
